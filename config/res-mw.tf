0e9a73d1a297a53ba54faad23b4b617d57bf549a
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2016-01-25 22:39:45 +0100

    disable provisioning

--- config/res-mw.tf
+++ config/res-mw.tf
@@ -1,8 +1,8 @@
 resource "digitalocean_droplet" "mw" {
-  count = 0
+  count = 30
   image = "centos-7-0-x64"
   name = "mw${count.index}"
-  region = "ams3"
+  region = "ams2"
   size = "1gb"
   private_networking = true
   ipv6 = true
@@ -12,29 +12,29 @@ resource "digitalocean_droplet" "mw" {
 
   depends_on = [
     "digitalocean_droplet.saltmaster",
-    "digitalocean_droplet.db"
+    #"digitalocean_droplet.db"
   ]
 
-  provisioner "local-exec" {
-    command = "sleep 120"
-  }
-
-  connection {
-      user = "root"
-      type = "ssh"
-      key_file = "${var.pvt_key}"
-      timeout = "5m"
-      agent = false
-  }
-
-  provisioner "file" {
-    source = "provision.sh"
-    destination = "/tmp/terraform-provision.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/terraform-provision.sh && /tmp/terraform-provision.sh ${digitalocean_droplet.saltmaster.ipv4_address_private}"
-    ]
-  }
+#  provisioner "local-exec" {
+#    command = "sleep 180"
+#  }
+#
+#  connection {
+#      user = "root"
+#      type = "ssh"
+#      key_file = "${var.pvt_key}"
+#      timeout = "5m"
+#      agent = false
+#  }
+#
+#  provisioner "file" {
+#    source = "provision.sh"
+#    destination = "/tmp/terraform-provision.sh"
+#  }
+#
+#  provisioner "remote-exec" {
+#    inline = [
+#      "chmod +x /tmp/terraform-provision.sh && /tmp/terraform-provision.sh ${digitalocean_droplet.saltmaster.ipv4_address_private}"
+#    ]
+#  }
 }
